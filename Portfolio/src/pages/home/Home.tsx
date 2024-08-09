8e60d25343e7339e2b578c9be19ac08fc048cfec
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2024-08-09 15:21:35 +0300

    Add main image

--- Portfolio/src/pages/home/Home.tsx
+++ Portfolio/src/pages/home/Home.tsx
@@ -5,29 +5,17 @@ import Image from '../../components/media/image';
 
 const Home: React.FC = () => {
   const logoSource = 'resources/media/logo/sfx-stjuice_site.png';
+  const mainImageSource = 'resources/media/images/alien-body.jpg';
+
   return (
     <div className={styles.home}>
       <header className={styles.logo}>
         <Image src={logoSource} alt='logo for special effects website' />
       </header>
 
-      <section className={styles.showcase}>
-        <h2>Featured Work</h2>
-        <div className={styles.carousel}>
-          {/* ImageCarousel component */}
-        </div>
-      </section>
-
-      <section className={styles.categories}>
-        <h2>Categories</h2>
-        <div className={styles.imageGrid}>
-          {/* ImageGrid component with links to Light, Moderate, and Epic categories */}
-        </div>
-      </section>
-
-      <footer className={styles.footer}>
-        <p>Contact me for custom SFX makeup requests!</p>
-      </footer>
+      <div className={styles.mainImage}>
+        <Image src={mainImageSource} alt='home image' />
+      </div>
     </div>
   );
 }
