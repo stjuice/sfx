85d9d7f8ee43b230f36fa5855490c7c612f35605
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2024-08-18 19:41:11 +0300

    Existing Home components design

--- Portfolio/src/pages/home/Home.tsx
+++ Portfolio/src/pages/home/Home.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import styles from './Home.module.scss';
+import testStyles from './Home-test.module.scss';
 
 import Image from 'components/media/image';
-import Text from 'components/text';
 import { GreetingMessage } from 'components/text/greetingMessage';
 
 const Home: React.FC = () => {
-  const logoSource = 'resources/media/logo/sfx-stjuice_site.png';
+  const logoSource = 'resources/media/logo/sfx-stjuice_site.svg';
   const mainImageSource = 'resources/media/images/alien-body.jpg';
 
   return (
-    <div className={styles.home}>
-      <header className={styles.header}>
-        <div className={styles.logo}>
-          <Image src={logoSource} alt='logo for special effects website' />
-        </div>
-        <div className={styles.mainImage}>
-          <Image src={mainImageSource} alt='home image' />
-        </div>
-      </header>
-      
-      <GreetingMessage />
+    <div className={testStyles.home}>
+      <div className={testStyles.logo}>
+        <Image src={logoSource} alt='logo for special effects website' />
+      </div>
+      <div className={testStyles.mainImage}>
+        <Image src={mainImageSource} alt='home image' />
+      </div>
+      <div className={testStyles.girlImage}>
+        <Image src={'resources/media/images/IMG_9108.jpg'} alt='image with girl in horns makeup' />
+      </div>
+      <div className={testStyles.content}>
+        <GreetingMessage />
+      </div>
     </div>
   );
 }
