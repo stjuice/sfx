4cf730035f692ff94c68ce1add6f52b45f084faa
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2024-08-18 19:49:37 +0300

    Add text  keys constant for easely reach text

---
+++ Portfolio/src/components/text/textKeys.ts
@@ -0,0 +1,16 @@
+export default {
+  WELCOME_TITLE: "WelcomeTitle",
+  HELLO: "Hello",
+  NAME: "Name",
+  PORTFOLIO_TITLE: "PortfolioTitle",
+  PORTFOLIO_DESCRIPTION: "PortfolioDescription",
+
+  PERSONALIZED: "Personalized",
+  PERSONALIZED_DESCRIPTION: "PersonalizedDescription",
+  PROFESSIONAL: "Professional",
+  PROFESSIONAL_DESCRIPTION: "ProfessionalDescription",
+  QUALITY: "Quality",
+  QUALITY_DESCRIPTION: "QualityDescription",
+  UNIQUE: "Unique",
+  UNIQUE_DESCRIPTION: "UniqueDescription"
+}
