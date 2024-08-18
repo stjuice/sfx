42ec0aa8bf9830fc2fd3c85f7e1c5e8b8c6f540b
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2024-08-18 19:50:12 +0300

    Add Cart component

--- Portfolio/src/pages/home/Home.tsx
+++ Portfolio/src/pages/home/Home.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import styles from './Home.module.scss';
-import testStyles from './Home-test.module.scss';
 
 import Image from 'components/media/image';
 import { GreetingMessage } from 'components/text/greetingMessage';
+import Card from './Card';
+import { textKeys as T } from 'components/text'
 
 const Home: React.FC = () => {
   const logoSource = 'resources/media/logo/sfx-stjuice_site.svg';
   const mainImageSource = 'resources/media/images/alien-body.jpg';
 
   return (
-    <div className={testStyles.home}>
-      <div className={testStyles.logo}>
+    <div className={styles.home}>
+      <div className={styles.logo}>
         <Image src={logoSource} alt='logo for special effects website' />
       </div>
-      <div className={testStyles.mainImage}>
+      <div className={styles.mainImage}>
         <Image src={mainImageSource} alt='home image' />
       </div>
-      <div className={testStyles.girlImage}>
+      <div className={styles.girlImage}>
         <Image src={'resources/media/images/IMG_9108.jpg'} alt='image with girl in horns makeup' />
       </div>
-      <div className={testStyles.content}>
+      <div className={styles.content}>
         <GreetingMessage />
+      </div>
+      <div className={styles.backgroundSection}>
+        <Card titleKey={T.PERSONALIZED} contentKeys={T.PERSONALIZED_DESCRIPTION} />
       </div>
     </div>
   );
