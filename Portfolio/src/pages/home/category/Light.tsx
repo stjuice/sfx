fe3948d97ae2d2614f6440eed7a828a93b778100
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2024-08-23 13:43:38 +0300

    Add header title

--- Portfolio/src/pages/home/category/Light.tsx
+++ Portfolio/src/pages/home/category/Light.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './Category.module.scss';
+import { textKeys as T } from 'components/text'
+import TextBlock from 'components/text/textBlock/TextBlock';
 
 const CategoryLight = () => {
   return (
-    <div>
-      <h1>Category Light</h1>
+    <div className={styles.light}>
+      <div className={styles.header}>
+        <TextBlock textKeys={[T.LIGHT]} className={styles.title} />
+      </div>
       <p>This is the Category Light page.</p>
     </div>
   );
