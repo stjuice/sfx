08dd58eb2a65cb6b222bf748204b997cec1e5ba8
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2024-08-23 13:26:30 +0300

    Navigate to category light page

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+import { CategoryLight } from 'pages/home/category';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* Add routes for other pages */}
+        <Route path="/category-light" element={<CategoryLight />} />
       </Routes>
     </Router>
   );
---
+++ Portfolio/src/pages/home/category/Light.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CategoryLight = () => {
+  return (
+    <div>
+      <h1>Category Light</h1>
+      <p>This is the Category Light page.</p>
+    </div>
+  );
+};
+
+export default CategoryLight;
