d56cb510ce597a7fe2fb5577795bd57da96af3e3
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2024-08-24 21:16:09 +0300

    Category template

--- Portfolio/src/pages/home/category/Light.tsx
+++ Portfolio/src/pages/home/category/Light.tsx
@@ -3,6 +3,7 @@ import styles from './Category.module.scss';
 import { textKeys as T } from 'components/text'
 import TextBlock from 'components/text/textBlock/TextBlock';
 import Image from 'components/media/image';
+import LightTemplate from './LightTemplate';
 
 const imagesGroup1 = [
   'resources/media/images/light/0.jpg',
@@ -17,92 +18,25 @@ const imagesGroup2 = [
 ];
 
 const CategoryLight = () => {
-  const [group2MarginTop, setGroup2MarginTop] = useState('-700px');
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const windowHeight = window.innerHeight;
-
-      const threshold = windowHeight * 0.3;
-
-      if (scrollPosition > threshold) {
-        setGroup2MarginTop('0');
-      } else {
-        setGroup2MarginTop('-700px');
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  const lightKeys = {
+    category: T.LIGHT,
+    categoryDscr: T.LIGHT_DESCRIPTION,
+    features: T.LIGHT_FEATURES,
+    featuresBlood: T.LIGHT_FEATURES_BLOOD,
+    featuresBloodDscr: T.LIGHT_FEATURES_BLOOD_DESCRIPTION,
+    featuresDecoration: T.LIGHT_FEATURES_DECORATION,
+    featuresDecorationDscr: T.LIGHT_FEATURES_DECORATION_DESCRIPTION,
+    featuresPaint: T.LIGHT_FEATURES_PAINT,
+    featuresPaintDscr: T.LIGHT_FEATURES_PAINT_DESCRIPTION,
+    for: T.LIGHT_FOR,
+    forList: T.LIGHT_FOR_LIST,
+    forConclusion: T.LIGHT_FOR_CONCLUSION,
+    themeEx: T.LIGHT_THEME_EXAMPLES,
+    themeExList: T.LIGHT_THEME_EXAMPLES_LIST,
+  };
 
   return (
-    <div className={styles.light}>
-      <div className={styles.header}>
-        <TextBlock textKeys={[T.LIGHT]} className={styles.title} />
-        <TextBlock textKeys={[T.LIGHT_DESCRIPTION]} className={styles.description} />
-      </div>
-      <div className={styles.group1}>
-        {imagesGroup1.map((src, index) => (
-          <Image
-            key={index}
-            src={src}
-            className={`${styles.image}}`}
-            alt={`image example light category ${index}`}
-          />
-        ))}
-      </div>
-      <div className={styles.containerFeatures}>
-        <TextBlock textKeys={[T.LIGHT_FEATURES]} className={styles.sectionHeader} />
-        <div className={styles.card}>
-          <div className={styles.features}>
-            <div className={styles.feature}>
-              <TextBlock textKeys={[T.LIGHT_FEATURES_BLOOD, T.SYMBOL_COLON]} className={styles.title} />
-              <TextBlock textKeys={[T.LIGHT_FEATURES_BLOOD_DESCRIPTION]} className={styles.description} />
-            </div>
-            <div className={styles.feature}>
-              <TextBlock textKeys={[T.LIGHT_FEATURES_DECORATION, T.SYMBOL_COLON]} className={styles.title} />
-              <TextBlock textKeys={[T.LIGHT_FEATURES_DECORATION_DESCRIPTION]} className={styles.description} />
-            </div>
-            <div className={styles.feature}>
-              <TextBlock textKeys={[T.LIGHT_FEATURES_PAINT, T.SYMBOL_COLON]} className={styles.title} />
-              <TextBlock textKeys={[T.LIGHT_FEATURES_PAINT_DESCRIPTION]} className={styles.description} />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.containerFor}>
-        <TextBlock textKeys={[T.LIGHT_FOR]} className={styles.sectionHeader} />
-        <div className={`${styles.card} ${styles.cardFor}`}>
-          <div className={styles.for}>
-            <TextBlock textKeys={[T.LIGHT_FOR_LIST]} className={styles.list} />
-          </div>
-          <div className={styles.for}>
-            <TextBlock textKeys={[T.LIGHT_FOR_CONCLUSION]} className={styles.conclusion} />
-          </div>
-        </div>
-      </div>
-      <div className={styles.examples}>
-        <TextBlock textKeys={[T.LIGHT_THEME_EXAMPLES]} className={styles.sectionHeader} />
-        <div className={styles.examplesList}>
-          <TextBlock textKeys={[T.LIGHT_THEME_EXAMPLES_LIST]} />
-        </div>
-      </div>
-      <div className={styles.group2} style={{ marginTop: group2MarginTop }}>
-        {imagesGroup2.map((src, index) => (
-          <Image
-            key={index + 3}
-            src={src}
-            className={`${styles.image}`}
-            alt={`image example light category ${index + imagesGroup1.length}`}
-          />
-        ))}
-      </div>
-    </div >
+    <LightTemplate imagesGroup1={imagesGroup1} imagesGroup2={imagesGroup2} textKeys={lightKeys} className={styles.light} />
   );
 };
 
